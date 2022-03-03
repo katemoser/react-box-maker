aa67083b758228fb2127a9c165c3569df6fad658
Peter Loft <peterloft3@gmail.com>
2022-03-03 14:19:45 -0600

    completed baseline NewBoxForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import Box from "./Box";
+import NewBoxForm from "./NewBoxForm"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NewBoxForm />
       <Box backgroundColor="red" width="100" height="200" />
     </div>
   );
--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 /**
  * props: addBox -- function that lives in BoxList, takes formData
  * 
@@ -6,6 +8,74 @@
  * hierarchy:
  * BoxList -> NewBoxForm
  */
-function NewBoxForm(){
+function NewBoxForm({ addBox }) {
+  const initialState = {
+    height: "",
+    width: "",
+    color: ""
+  };
+
+  const [formData, setFormData] = useState(initialState);
+
+  function handleChange(evt) {
+    const fieldName = evt.target.name;
+    const value = evt.target.value;
+
+    setFormData(currData => {
+      currData[fieldName] = value
+      return { ...currData }
+    });
+  }
+
+
+
+  /** Send {name, quantity} to parent
+   * & clear form. */
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    addBox(formData);
+    setFormData(initialState);
+
+  }
+
+  return (
+    <form className="BoxMaker-NewBox-Form">
+      New Box Form
+      <p/>
+      <label htmlFor="height-input">Height:</label>
+      <input
+        id="height-input"
+        onChange={handleChange}
+        value={formData.height}
+        name="height"
+        >
+
+      </input>
+      <p/>
+      <label htmlFor="width-input">Width:</label>
+      <input
+        id="width-input"
+        onChange={handleChange}
+        value={formData.width}
+        name="width"
+        >
+
+      </input>
+      <p/>
+      <label htmlFor="color-input">Background Color:</label>
+      <input
+        id="color-input"
+        onChange={handleChange}
+        value={formData.color}
+        name="color"
+        >
+
+      </input>
+      <p/>
+      <button onClick={handleSubmit}>Add new box!</button>
+    </form>
+  )
 
 }
+
+export default NewBoxForm;
